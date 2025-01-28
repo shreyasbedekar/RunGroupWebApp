7b826b81023d47c88e32af2b6249969a3982e93a
Shreyas Bedekar <bedesn01@pfw.edu>
2025-01-27 19:29:46 -0500

    Models Created

---
+++ RunGroupWebApp/Data/Enum/ClubCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace RunGroupWebApp.Data.Enum
+{
+    public enum ClubCategory
+    {
+        Running,
+        Biking,
+        Swimming,
+        Hiking,
+        Walking,
+        Yoga,
+        Pilates,
+        CrossFit,
+        WeightLifting,
+        Other
+    }
+}
---
+++ RunGroupWebApp/Data/Enum/RaceCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace RunGroupWebApp.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        HalfMarathon,
+        FiveK,
+        TenK,
+    }
+}
