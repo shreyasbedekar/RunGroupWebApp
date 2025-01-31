30356f814afc8248891cd981c28e5ac0cac7bced
Shreyas Bedekar <bedesn01@pfw.edu>
2025-01-30 23:13:27 -0500

    Detail Pages

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context) 
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Club club = _context.Clubs.Include(a=> a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
         }
     }
 }
